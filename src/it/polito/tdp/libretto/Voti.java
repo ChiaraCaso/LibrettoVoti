3cd816849330dec14402ff006b98e842d7da9639
Caso <Caso@172.21.57.92>
2019-03-11 13:52:52 +0100

    Aggiunti metodi standard

--- src/it/polito/tdp/libretto/Voti.java
+++ src/it/polito/tdp/libretto/Voti.java
@@ -1,5 +1,15 @@
 package it.polito.tdp.libretto;
 
+import java.time.LocalDate;
+
 public class Voti {
+	private int voto ;
+	private string corso ;
+	private LocalDate data;  //Date e Calendar erano gli unici modi per gestire le date fino a java 6, da java 7 esiste la libreria java time che contiene local date
+	
+	//facciamo in modo di portare questo progetto sottoforma di version
+	//clicco con il tasto destro sul progetto e vado su team e poi su share project e seleziono le spunte.
+	//clicco di nuovo con il tasto destro sul progetto, vado su team e poi su commit che mi permette di vedere le ultime modifiche del file
+	
 
 }
