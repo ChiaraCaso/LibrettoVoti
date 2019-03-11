f9eb4af479848dfc07e6cc44eee89bdee2112748
Caso <Caso@172.21.57.92>
2019-03-11 14:04:28 +0100

    ciao

--- src/it/polito/tdp/libretto/Voti.java
+++ src/it/polito/tdp/libretto/Voti.java
@@ -4,12 +4,19 @@ import java.time.LocalDate;
 
 public class Voti {
 	private int voto ;
-	private string corso ;
+	private String corso ;
 	private LocalDate data;  //Date e Calendar erano gli unici modi per gestire le date fino a java 6, da java 7 esiste la libreria java time che contiene local date
+	
+	public Voti(int voto, String corso, LocalDate data) {
+		super();
+		this.voto = voto;
+		this.corso = corso;
+		this.data = data;
+	}
 	
 	//facciamo in modo di portare questo progetto sottoforma di version
 	//clicco con il tasto destro sul progetto e vado su team e poi su share project e seleziono le spunte.
 	//clicco di nuovo con il tasto destro sul progetto, vado su team e poi su commit che mi permette di vedere le ultime modifiche del file
 	
-
+	
 }
